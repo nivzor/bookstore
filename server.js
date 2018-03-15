df52388b3fe86318b6b3ac564e31dde2ef65400c
NivO <nivchuk@gmail.com>
2018-03-15 16:32:20 +0200

    fixed server.js setup

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 let app = express();
 const PORT = process.env.PORT || 9000;
-app.use(express.static('public'));
+
+app.use(express.static(path.resolve(__dirname, './')));
+
 app.listen(PORT, function(){
 	console.log('Express server is up on port: ' + PORT);
 });
